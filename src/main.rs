863beb7d6c06b3d4ae24b0e4e4924bc8526ccd3c
Victor <victorbonatoit@gmail.com>
2023-12-25 19:15:58 -0300

    create main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use clap::Parser;
+use posts_axum::config::{self, Config};
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    env_logger::init();
+
+    let config = Config::parse();
+
+    let db = PgPoolOptions::new()
+        .max_connections(50)
+        .connect(&config.database_url)
+        .await
+        .context("could not connect to database")?;
+
+    // TODO: serve http
+
+    Ok(())
 }
